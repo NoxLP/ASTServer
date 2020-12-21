d81114e77a84a8b567c65cc70375f855e670e73c
Norber <nox_nox_@hotmail.com>
2020-12-21 01:37:21 +0000

    Added body-parser via npm. GET and POST done.

--- app.js
+++ app.js
@@ -1,9 +1,90 @@
-const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/alwSaveTabs', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to Mongo!');
+  }).catch(err => {
+    console.error('Error connecting to mongo!', err);
+  })
+
 const WindowsModel = require('./model/model')
 
-app
-  .get('/windows', (req, res, next) => {
-    
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+//get all windows
+app.get('/windows', (req, res, next) => {
+  console.log('\nRequested all windows through "get/windows"')
+
+  WindowsModel.find({})
+    .then(resp => {
+      console.log('Found all windows in db', resp)
+
+      res.json(resp)
+    })
+    .catch(err => {
+      console.error(`Error when trying to respond to "get/windows":\n${err}`)
+    })
+})
+
+app 
+  //new window
+  .post('/window', (req, res, next) => {
+    console.log('\nPosted new window ', req.body)
+
+    WindowsModel.create(req.body)
+      .then(resp => {
+        console.log('Created new window')
+
+        res.status(201)
+        res.send('Stored new window')
+      })
+      .catch(err => {
+        console.error(`Error when trying to create new window in "post/window":\n${err}`)
+      })
   })
+  //new tab from window
+  .post('/tab', async (req, res, next) => {
+    console.log('\nPosted new tab ', req.body)
+
+    let foundWindows = await WindowsModel.find(req.query)
+    console.log('found windows ', foundWindows)
+    if(foundWindows.length === 0) {
+      console.log('No window found with the given parameters')
+
+      res.status(404)
+      res.send('No window found with the given parameters')
+    } else if(foundWindows.length > 1) {
+      console.error('Found multiple windows with given criteria')
+
+      res.status(400)
+      res.send(`Found multiple windows with given criteria`)
+    } else {
+      console.log('windows found')
+
+      foundWindows[0].tabs.push(req.body)
+      console.log(foundWindows[0].tabs)
+
+      foundWindows[0].save()
+        .then(resp => {
+          console.log('saved updated window')
+
+          res.status(201)
+          res.send(`Stored tab in window\n${JSON.stringify(foundWindows[0], null, 4)}`)
+        })
+        .catch(err => {
+          console.log('error saving updated window: ', err)
+
+          res.status(500)
+          res.send(`Error saving updated window:\n\n${err}`)
+        })
+    }
+  })
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server started at port 3000')
+})
